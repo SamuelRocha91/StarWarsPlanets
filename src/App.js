88845c1e3786f916c045ec46ef2b8a36c7f18d70
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-02-27 17:26:36 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
+import DataContext from './context/DataContext';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets').then((response) => response.json()).then((data) => {
+      const planetsApi = data.results.map(({
+        name,
+        rotation_period: rotationPeriod,
+        orbital_period: orbitalPeriod,
+        diameter,
+        climate,
+        gravity,
+        terrain,
+        surface_water: surfaceWater,
+        population,
+        films,
+        created,
+        edited,
+        url,
+      }) => ({
+        name,
+        rotationPeriod,
+        orbitalPeriod,
+        diameter,
+        climate,
+        gravity,
+        terrain,
+        surfaceWater,
+        population,
+        films,
+        created,
+        edited,
+        url,
+      }));
+      setPlanets(planetsApi);
+    });
+  }, []);
+  const values = useMemo(() => ({
+    planets,
+  }), [planets]);
   return (
-    <span>Hello, App!</span>
+    <DataContext.Provider value={ values }>
+      <Table />
+    </DataContext.Provider>
   );
 }
 
---
+++ src/components/Table.jsx
@@ -0,0 +1,62 @@
+import { useContext } from 'react';
+import DataContext from '../context/DataContext';
+
+function Table() {
+  const { planets } = useContext(DataContext);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>created</th>
+          <th>Edited</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map(({
+          name,
+          rotationPeriod,
+          orbitalPeriod,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surfaceWater,
+          population,
+          films,
+          created,
+          edited,
+          url,
+        }) => (
+          <tr key={ name }>
+            <td>{name}</td>
+            <td>{rotationPeriod}</td>
+            <td>{orbitalPeriod}</td>
+            <td>{diameter}</td>
+            <td>{climate}</td>
+            <td>{gravity}</td>
+            <td>{terrain}</td>
+            <td>{surfaceWater}</td>
+            <td>{population}</td>
+            <td>{films}</td>
+            <td>{created}</td>
+            <td>{ edited }</td>
+            <td>{ url }</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
